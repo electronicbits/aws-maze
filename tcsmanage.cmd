fc9a9b43db4d76a8770a2f321d472d73740addb5
Dario Alvarez <dario@electronic-bits.com>
2015-08-30 21:18:42 +1000

    adding team city start stop script

---
+++ tcsmanage.cmd
@@ -0,0 +1,29 @@
+@ECHO OFF
+IF "%1"=="install" GOTO install
+IF "%1"=="uninstall" GOTO uninstall
+IF "%1"=="start" GOTO start
+IF "%1"=="stop" GOTO stop
+GOTO end
+
+:install
+SETX TEAMCITY_DATA_PATH C:\TeamCityServerData /m
+XCOPY C:\TeamCity-9.1.1 C:\TeamCityServer /s /q /i
+NETSH advfirewall firewall add rule name="TeamCity Server" dir=in action=allow protocol=TCP localport=80
+CALL C:\TeamCityServer\bin\teamcity-server service install /runAsSystem
+GOTO end
+
+:uninstall
+CALL C:\TeamCityServer\bin\teamcity-server service delete
+NETSH advfirewall firewall delete rule name="TeamCity Server"
+RMDIR /s /q C:\TeamCityServer
+GOTO end
+
+:start
+NET start TeamCity
+GOTO end
+
+:stop
+NET stop TeamCity
+GOTO end
+
+:end
