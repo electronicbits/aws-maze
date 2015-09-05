b7c7e337d589171724c494a1c65132d9810032dd
Dario Alvarez <dario@electronic-bits.com>
2015-09-05 18:35:57 +1000

    adding main powershell script

---
+++ installTeamCity.ps1
@@ -0,0 +1,47 @@
+#Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName -Filter "Name like 'Java%'"
+#Get-WmiObject -Class Win32_Product  -Filter "Name like 'Java%'" | Select -Expand Version
+#Start-Process "C:\\Downloads\jdk-8u60-windows-x64\\jdk-8u60-windows-x64.exe /s /L jdk_install.log" -Wait
+
+C:\\Java8.6\\jdk-8u60-windows-x64\\jdk-8u60-windows-x64.exe /s /L jdk_install.log
+
+$JavaExists = $false
+
+while($JavaExists -eq $false){
+    try{
+        start-process  javad  -ArgumentList "-version" -NoNewWindow
+        $JavaExists = $true
+    }
+    catch{
+        $JavaExists = $false
+        
+        $ErrorMessage = $_.Exception.Message
+        $FailedItem = $_.Exception.ItemName
+
+        $Time=Get-Date
+        "$Time -- Java still not installed, waiting 5 secs " | out-file installTeamCity.log -append
+
+        Start-Sleep -s 5
+    }
+}
+
+try{
+    $Time = Get-Date
+    "$Time -- Installing teamcity " | out-file installTeamCity.log -append
+    tcsmanage.cmd install
+}
+catch{
+    "$Time -- Error trying to install TeamCity " | out-file installTeamCity.log -append
+    Exit
+}
+
+try{
+    $Time = Get-Date
+    "$Time -- Installing teamcity " | out-file installTeamCity.log -append
+    tcsmanage.cmd start
+}
+catch{
+    "$Time -- Error trying to start TeamCity " | out-file installTeamCity.log -append
+    Exit
+}
+
+
